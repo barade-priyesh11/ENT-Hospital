7f3b7671f881b8b2c8e5ba054582eaf8fb67ec11
shivam <shivam96ti@gmail.com>
2024-03-06 12:22:50 +0530

    create home page for ent website

---
+++ index.js
@@ -0,0 +1,26 @@
+$('.round').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('.arrow').toggleClass('bounceAlpha');
+});
+
+$(document).ready(function(){
+    $('.progress-title > span').each(function(){
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        },{
+            duration: 1500,
+            easing: 'swing',
+            step: function (now){
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
+
+
+
+
+
+
+
